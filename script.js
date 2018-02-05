34815fc8c84c6acd9ffabe9095456df0c3e67d3c
bmtreuherz <bmtreuherz@ufl.edu>
2018-02-05 14:02:33 -0500

    Added a comment

--- script.js
+++ script.js
@@ -10,6 +10,40 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.send(null); // null body in the request (our query params are in the url)
 }
 
+// This function is called when the search button is clicked.
+function onSearchBtnClicked(){
+  // Clear the Table (Remove everything except the first row -header)
+  $("#search-results-table").find("tr:gt(0)").remove()
+
+  var itunesBaseUrl = "https://itunes.apple.com/search"
+
+  // Find the key term from the searchInput
+  var keyterm = $('#search-input').val()
+
+  // Search only for songs that include that key term.
+  var url = itunesBaseUrl + "?term=" + keyterm
+  url += "&kind=song"
+
+  httpGetAsync(url, function(response){
+    var jsonResponse = JSON.parse(response)
+    buildTableFromResults(jsonResponse.results)
+  })
+}
+
+// Builds the table from the json responseText
+function buildTableFromResults(results){
+  results.forEach(function(result){
+    var tableRow = buildTableRow(result)
+    $('#search-results-table tr:last').after(tableRow)
+  })
+}
+
+$(document).ready(function() {
+    $("#search-btn").click(function(){
+        onSearchBtnClicked()
+    })
+})
+
 function millisToMinutesAndSeconds(millis) {
   var minutes = Math.floor(millis / 60000)
   var seconds = Math.round((millis % 60000) / 1000)
@@ -30,41 +64,6 @@ function buildTableRow(rowItem){
   row += "</tr>"
   return row
 }
-
-// Builds the table from the json responseText
-function buildTableFromResults(results){
-  results.forEach(function(result){
-    var tableRow = buildTableRow(result)
-    $('#search-results-table tr:last').after(tableRow)
-  })
-}
-
-
-// This function is called when the search button is clicked.
-function onSearchBtnClicked(){
-  // Clear the Table
-  $("#search-results-table").find("tr:gt(0)").remove()
-
-  var itunesBaseUrl = "https://itunes.apple.com/search"
-
-  // Find the key term from the searchInput
-  var keyterm = $('#search-input').val()
-
-  // Search only for songs that include that key term.
-  var url = itunesBaseUrl + "?term=" + keyterm
-  url += "&kind=song"
-
-  httpGetAsync(url, function(response){
-    var jsonResponse = JSON.parse(response)
-    buildTableFromResults(jsonResponse.results)
-  })
-}
-
-$(document).ready(function() {
-    $("#search-btn").click(function(){
-        onSearchBtnClicked()
-    })
-})
 
 // Moving forward
 // There is a lot more data coming back than what were are showing. Try logging the response to see what
